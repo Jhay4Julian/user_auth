a16e10089ca9c0e4f7c7f2152c9b3e5ecb83846a
Julian Koripamo <jkoripamo@gmail.com>
2023-11-11 16:27:36 +0100

    login screen ui

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:user_auth/components/my_button.dart';
+import 'package:user_auth/components/my_textfield.dart';
+import 'package:user_auth/components/square_tile.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // text editing contollers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            // logo
+            const Icon(
+              Icons.lock,
+              size: 100,
+            ),
+            const SizedBox(height: 50),
+
+            Text(
+              'Welcome back!',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // email textfield
+            MyTextField(
+              controller: emailController,
+              hintText: 'Email',
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            // password textfield
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 10),
+
+            // forgot password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // sign in button
+            MyButton(
+              title: 'Sign In',
+              onTap: signUserIn,
+            ),
+
+            const SizedBox(height: 25),
+
+            // or continue with
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                    thickness: 0.5,
+                    color: Colors.grey[400],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'Or continue with',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Divider(
+                    thickness: 0.5,
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 50),
+
+            // google and apple sign in button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // google button
+                SquareTile(
+                  imagePath: 'assets/images/search.png',
+                  onTap: () {},
+                ),
+
+                const SizedBox(width: 25),
+
+                // apple button
+                SquareTile(
+                  imagePath: 'assets/images/apple-logo.png',
+                  onTap: () {},
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 50),
+
+            // dont have an account? sign up
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account?",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(width: 4),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
