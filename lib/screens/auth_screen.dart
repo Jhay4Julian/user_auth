db0a6beb2bda57325983a4ee1ceb6b298ff90d0c
Julian Koripamo <jkoripamo@gmail.com>
2023-11-16 18:33:33 +0100

    update not logged in command

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:user_auth/screens/login_screen.dart';
+import 'package:user_auth/screens/login_or_register_screen.dart';
 
 import 'home_screen.dart';
 
@@ -20,7 +20,7 @@ class AuthScreen extends StatelessWidget {
 
           // user is NOT logged in
           else {
-            return LoginScreen();
+            return LoginOrRegisterScreen();
           }
         },
       ),
