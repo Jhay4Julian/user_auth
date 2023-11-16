66689df8932f6c51ca4881ca36f64c0dba20e9e6
Julian Koripamo <jkoripamo@gmail.com>
2023-11-16 18:37:05 +0100

    update ui

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -69,137 +69,139 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-            // logo
-            const Icon(
-              Icons.lock,
-              size: 100,
-            ),
-            const SizedBox(height: 50),
-
-            Text(
-              'Welcome back!',
-              style: TextStyle(
-                color: Colors.grey[700],
-                fontSize: 16,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
               ),
-            ),
+              const SizedBox(height: 50),
 
-            const SizedBox(height: 25),
-
-            // email textfield
-            MyTextField(
-              controller: emailController,
-              hintText: 'Email',
-              obscureText: false,
-            ),
-
-            const SizedBox(height: 10),
-
-            // password textfield
-            MyTextField(
-              controller: passwordController,
-              hintText: 'Password',
-              obscureText: true,
-            ),
-
-            const SizedBox(height: 10),
-
-            // confirm password textfield
-            MyTextField(
-              controller: confirmPasswordController,
-              hintText: 'Confirm Password',
-              obscureText: true,
-            ),
-
-            const SizedBox(height: 25),
-
-            // Register button
-            MyButton(
-              title: 'Register',
-              onTap: signUserUp,
-            ),
-
-            const SizedBox(height: 25),
-
-            // or register with
-            Row(
-              children: [
-                Expanded(
-                  child: Divider(
-                    thickness: 0.5,
-                    color: Colors.grey[400],
-                  ),
+              Text(
+                'Create an account.',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    'Or register with',
+              ),
+
+              const SizedBox(height: 25),
+
+              // email textfield
+              MyTextField(
+                controller: emailController,
+                hintText: 'Email',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              // confirm password textfield
+              MyTextField(
+                controller: confirmPasswordController,
+                hintText: 'Confirm Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 25),
+
+              // Register button
+              MyButton(
+                title: 'Register',
+                onTap: signUserUp,
+              ),
+
+              const SizedBox(height: 25),
+
+              // or register with
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'Or register with',
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 50),
+
+              // google and apple sign in button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // google button
+                  SquareTile(
+                    imagePath: 'assets/images/search.png',
+                    onTap: () {},
+                  ),
+
+                  const SizedBox(width: 25),
+
+                  // apple button
+                  SquareTile(
+                    imagePath: 'assets/images/apple-logo.png',
+                    onTap: () {},
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 50),
+
+              // already have an account? sign in
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
                     style: TextStyle(
                       color: Colors.grey[700],
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Divider(
-                    thickness: 0.5,
-                    color: Colors.grey[400],
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 50),
-
-            // google and apple sign in button
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // google button
-                SquareTile(
-                  imagePath: 'assets/images/search.png',
-                  onTap: () {},
-                ),
-
-                const SizedBox(width: 25),
-
-                // apple button
-                SquareTile(
-                  imagePath: 'assets/images/apple-logo.png',
-                  onTap: () {},
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 50),
-
-            // already have an account? sign in
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account?",
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                  ),
-                ),
-                const SizedBox(width: 4),
-                TextButton(
-                  onPressed: widget.onPressed,
-                  child: const Text(
-                    'Sign In',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
+                  const SizedBox(width: 4),
+                  TextButton(
+                    onPressed: widget.onPressed,
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
